0484136b1d8a45efd22fb4058680653bc9ce376e
Naeslas7 <alibert.nico@gmail.com>
2014-04-30 09:32:35 +0200

    Création des méthodes de partition

--- src/Partition.java
+++ src/Partition.java
@@ -1,4 +1,32 @@
 
 public class Partition {
+	
+	//Attribut de la classe
+	String cheminFichierAudio;
+	String nom;
+	//TODO voir pour le tableau ? Liste Chainée ? TableauNote
+	TripletNote[] tableauNote;
+	//TODO voir pour le tableau ? Liste Chainée ? TableauScore
+	Score[] tableauScore;
+	
+	//Constructeur
+	//TODO Implémenter le constructeur
+	public Partition(){
 
+	}
+	//Permet d'accéder à toutes les notes de la partition
+	//TODO Définir le type retourner + Implémentation
+	public void accessNote(){
+
+	}
+	//Permet d'accéder à tous les scores de la partition
+	//TODO Définir le type retourner + Implémentation
+	public void accessScore(){
+
+	}
+	//Permet d'ajouter un score
+	//TODO Définir le type retourner + Implémentation
+	public void addScore(){
+
+	}
 }
