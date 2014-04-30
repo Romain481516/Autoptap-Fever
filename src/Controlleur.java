49a4cd62c24926f4d8c92b0a9b8686b279543c24
Naeslas7 <alibert.nico@gmail.com>
2014-04-30 10:29:39 +0200

    Début de l'implémentation du constructeur de la classe Partition.

--- src/Controlleur.java
+++ src/Controlleur.java
@@ -1,4 +1,3 @@
-
 public class Controlleur {
 	Bibliotheque biblio;
 
--- src/Partition.java
+++ src/Partition.java
@@ -1,6 +1,13 @@
+import java.io.InputStream;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 public class Partition {
 	
 	//Attribut de la classe
+	//TODO Supprimer le fichier exemple
+	//String cheminPartitionXML = "XMLSchema2ex.xml";
 	String cheminFichierAudio;
 	String nom;
 	//TODO voir pour le tableau ? Liste Chainée ? TableauNote
@@ -10,8 +17,19 @@ public class Partition {
 	
 	//Constructeur
 	//TODO Implémenter le constructeur
-	public Partition(){
-
+	public Partition(String cheminPartXML){
+		//Appelle du parser SAX sur le fichier XML Spécifié
+		try{
+			SAXParserFactory factory = SAXParserFactory.newInstance(); 
+			SAXParser saxParser = factory.newSAXParser();
+			InputStream xmlStream = Partition.class.getResourceAsStream(cheminPartXML);
+			saxParser.parse(xmlStream, new SaxHandler());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//Affectation des attributs nom et cheminFichierAudio
+		
+		
 	}
 	//Permet d'accéder à toutes les notes de la partition
 	//TODO Définir le type retourner + Implémentation
