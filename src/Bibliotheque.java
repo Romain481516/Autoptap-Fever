01914ca4d43a2a7251798fa646e10778fd9a0600
Naeslas7 <alibert.nico@gmail.com>
2014-04-30 09:44:33 +0200

    Creation des méthodes de Bibliothèque

--- src/Bibliotheque.java
+++ src/Bibliotheque.java
@@ -1,4 +1,28 @@
 
 public class Bibliotheque {
-
+	//Attribut de la classe
+	//TODO Organisation de la liste de partition - Ajout ? Accès par lecture ? Regénérer à chaque fois - Booléen de modification ?
+	Partition[] listePartition;
+	
+	//TODO Implémenter le constructeur - Attribut/
+	public Bibliotheque(){
+		
+	}
+	//TODO Implémenter la vérification des partitions par rapport au Schema XML
+	//Vérifie l'intégrité des partitions
+	public void VerifIntegritePartition(){
+		
+	}
+	//TODO Implémenter l'ajout d'une partition
+	public void addPartition(){
+		
+	}
+	//TODO Implémenter la suppression d'une partition de la bibliothèque
+	public void deletePartition(){
+		
+	}
+	//TODO Choisir le type retournée
+	public void allScore(){
+		
+	}
 }
--- src/Partition.java
+++ src/Partition.java
@@ -1,4 +1,3 @@
-
 public class Partition {
 	
 	//Attribut de la classe
