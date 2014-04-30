e360f36f3d0cdfe54289debc252c5f28c91c8c4f
Naeslas7 <alibert.nico@gmail.com>
2014-04-30 09:16:48 +0200

    Creation des méthodes de la classe TripletNote.

--- src/TripletNote.java
+++ src/TripletNote.java
@@ -1,4 +1,32 @@
-
 public class TripletNote {
 
+	//Attribut de la classe TripletNote
+	boolean[] tripletNote = new boolean[3];
+	int duree;
+	int debut;
+	
+	//Constructeur
+	//TODO écrire le constructeur
+	public TripletNote(){
+		
+	}
+	//Methode qui attribut une valeur (true ou false) au touche.
+	//TODO choisir attribut - Voir avec le Parser Sax
+	public void setTouche(){
+	}
+	//TODO choisir attribut - Voir avec le Parser Sax
+	public void setDebut(){
+	}
+	//TODO choisir attribut - Voir avec le Parser Sax
+	public void setduree(){
+	}
+	//TODO choisir ce que renvoie la méthode.
+	public void getTouche(){
+	}
+	//TODO choisir ce que renvoie la méthode.
+	public void getDuree(){
+	}
+	//TODO choisir ce que renvoie la méthode.
+	public void getDebut(){
+	}
 }
